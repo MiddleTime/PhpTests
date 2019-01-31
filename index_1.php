e90723b34784cd2d9a3164fb2e60b0048bfb64c6
MiddleTime <nadoroge@gmail.com>
2019-01-31 13:51:51 +0200

    Testing2

---
+++ index_1.php
@@ -0,0 +1,7 @@
+<?php 
+header("Content-Type: text/html; charset=utf-8");
+?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<?php 
+echo "Привет, Мир"; 
+?>
---
+++ index_2.php
@@ -0,0 +1,30 @@
+<?php header("Content-Type: text/html;charset=utf-8");?>
+<meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
+<?
+$myVar = 0; //Целое число
+$myVar = 3.14; //Число с плавающей точкой
+$myVar = "Текущий год"; //Строка
+$myVar = true; //Булева переменная
+$myVar = array(250, 300, 325, 475); //Массив
+
+//++++++++++++++
+
+
+<?php header("Content-Type: text/html;charset=utf-8");?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<?php
+$myArray = array('Элемент1' => 'Значение1');
+
+?>
+
+
+
+<?php
+//это комментарий PHP только для данной строки
+?>
+
+<?php
+/*это начало нашего комментария PHP.
+Он может занимать несколько строк.
+Здесь он заканчивается.*/
+
