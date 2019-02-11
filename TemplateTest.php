0a164dcb7582c15c2420f053dad6adec8702740f
MiddleTime <nadoroge@gmail.com>
2019-02-11 13:01:44 +0200

    First

--- TemplateTest.php
+++ TemplateTest.php
@@ -1,4 +1,4 @@
-<?php header("Content-Type: text/html;charset=utf-8");?>
+<?php header("Content-Type: text/html;charset=utf-8"); ?>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8"/>
 <?php
 $pageTitle = 'Пример шаблона';
